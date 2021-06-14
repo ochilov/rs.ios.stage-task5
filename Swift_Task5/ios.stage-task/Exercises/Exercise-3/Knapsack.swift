6f73ffae877de169424d9837731a7188adc89310
JohnO <>
2021-06-14 20:54:00 +0300

    feat: Exercise-3: implement Knapsack

--- Swift_Task5/ios.stage-task/Exercises/Exercise-3/Knapsack.swift
+++ Swift_Task5/ios.stage-task/Exercises/Exercise-3/Knapsack.swift
@@ -17,6 +17,69 @@ public final class Knapsack {
     }
     
     func findMaxKilometres() -> Int {
-        return -1
+		var maxDistance = 0
+		
+		// drinks and food, sorted by higher scrore (distance/weight)
+		let optimalSortedDrinks: [Supply] = drinks.sorted {
+			$0.value/$0.weight > $1.value/$1.weight
+		}
+		let optimalSortedFoods: [Supply] = foods.sorted {
+			$0.value/$0.weight > $1.value/$1.weight
+		}
+		
+		// drinks and food, sorted by weight
+		var weightSortedDrinks: [Supply] = drinks.sorted { $0.weight < $1.weight }
+		weightSortedDrinks.insert((0, 0), at: 0)
+		
+		var weightSortedFoods: [Supply] = foods.sorted { $0.weight < $1.weight }
+		weightSortedFoods.insert((0, 0), at: 0)
+		
+		// look for data and find optimal value
+		for optimalDrink in optimalSortedDrinks {
+			drinksLoop:
+			for d in 0 ..< weightSortedDrinks.count {
+				var drinkWeight = optimalDrink.weight
+				var drinkDistance = optimalDrink.value
+				for di in d ..< weightSortedDrinks.count {
+					let drink = weightSortedDrinks[di]
+					// skip same items
+					if optimalDrink.weight == drink.weight && optimalDrink.value == drink.value {
+						continue
+					}
+					drinkWeight += drink.weight
+					drinkDistance += drink.value
+					if drinkWeight > maxWeight {
+						break drinksLoop
+					}
+					
+					for optimalFood in optimalSortedFoods {
+						foodsLoop:
+						for f in 0 ..< weightSortedFoods.count {
+							var foodWeight = optimalFood.weight
+							var foodDistance = optimalFood.value
+							for fi in f ..< weightSortedFoods.count {
+								let food = weightSortedFoods[fi]
+								// skip same items
+								if optimalFood.weight == food.weight && optimalFood.value == food.value {
+									continue
+								}
+								foodWeight += food.weight
+								foodDistance += food.value
+								if foodWeight + drinkWeight > maxWeight {
+									break foodsLoop
+								}
+								
+								let distance = min(foodDistance, drinkDistance)
+								if maxDistance < distance {
+									maxDistance = distance
+								}
+							}
+						}
+					}
+				}
+			}
+		}
+		
+        return maxDistance
     }
 }
